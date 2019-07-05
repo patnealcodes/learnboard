7a26e5b54eb0e1c9e999aa055e1ebb1005cd0c31
Patrick Neal <pat.neal@rocketinsights.com>
2019-07-05 16:05:23 -0400

    #initcommit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './Index.scss'
+
+const App = () => (
+  <div>LearnBoard</div>
+)
+
+ReactDOM.render(<App />, document.querySelector('#root'))
